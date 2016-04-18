55a748b6686447c0f39663cf3a2608b7a58fd464
Seren <git@seren.net>
2016-04-18 16:28:16 -0700

    Add root-user check and remove individual sudos

--- temps-simple.sh
+++ temps-simple.sh
@@ -2,11 +2,16 @@
 # Outputs drive and cpu temps
 # From the freenas.org forum, but origin unknown
 
+if [ "$(id -u)" != "0" ]; then
+  echo "Error: this script needs to be run as root (for smartctl). Try 'sudo $0'"
+  exit 1
+fi
+
 for i in $(/sbin/sysctl -n kern.disks | awk '{for (i=NF; i!=0 ; i--) if(match($i, '/ada/')) print $i }' );
 do
- DevTemp=`sudo /usr/local/sbin/smartctl -a /dev/$i | awk '/Temperature_Celsius/{print $0}' | awk '{print $10 "C"}'`;
- #DevSerNum=`sudo /usr/local/sbin/smartctl -a /dev/$i | awk '/Serial Number:/{print $0}' | awk '{print $3}'`;
- #DevName=`sudo /usr/local/sbin/smartctl -a /dev/$i | awk '/Device Model:/{print $0}' | awk '{print $3}'`;
+ DevTemp=`/usr/local/sbin/smartctl -a /dev/$i | awk '/Temperature_Celsius/{print $0}' | awk '{print $10 "C"}'`;
+ #DevSerNum=`/usr/local/sbin/smartctl -a /dev/$i | awk '/Serial Number:/{print $0}' | awk '{print $3}'`;
+ #DevName=`/usr/local/sbin/smartctl -a /dev/$i | awk '/Device Model:/{print $0}' | awk '{print $3}'`;
  echo "$i - $DevTemp $DevSerNum $DevName";
 done
 /sbin/sysctl -a |egrep -E "cpu\.[0-9]+\.temp"
