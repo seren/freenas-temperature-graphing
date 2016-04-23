997e074d2b4799b3e47680c450d02e104af3571a
Seren <git@seren.net>
2016-04-23 10:32:06 -0700

    Fix error when num devices > num colors...
    Use an index that can wrap back to zero, instead
    of the raw device count.

--- rrd-graph.sh
+++ rrd-graph.sh
@@ -29,7 +29,14 @@ SAFETEMPLINE=40
 # # Different strokes for different folks
 # LINECOLORS=( 0000FF 4573A7 AA4644 89A54E 71588F 006060 0f4880 )
 # LINECOLORS=( 0000FF FF4A46 008941 006FA6 A30059 FFDBE5 7A4900 0000A6 63FFAC B79762 004D43 8FB0FF 997D87 )
-LINECOLORS=( 1CE6FF FF34FF FF4A46 008941 A30059 7A4900 63FFAC B79762 004D43 8FB0FF 997D87 000000 )
+# LINECOLORS=( 1CE6FF FF34FF FF4A46 008941 A30059 7A4900 63FFAC B79762 004D43 8FB0FF 997D87 000000 )
+# From http://stackoverflow.com/questions/309149/generate-distinctly-different-rgb-colors-in-graphs
+# LINECOLORS=( 777A7E E0FEFD E16DC5 01344B F8F8FC 9F9FB5 182617 FE3D21 7D0017 822F21 EFD9DC 6E68C4 35473E 007523 767667 A6825D 83DC5F 227285 A95E34 526172 979730 756F6D 716259 E8B2B5 B6C9BB 9078DA 4F326E B2387B 888C6F 314B5F E5B678 38A3C6 586148 5C515B CDCCE1 C8977F )
+# From http://stackoverflow.com/questions/309149/generate-distinctly-different-rgb-colors-in-graphs
+LINECOLORS=( 000000 00FF00 0000FF FF0000 01FFFE FFA6FE FFDB66 006401 010067 95003A 007DB5 FF00F6 FFEEE8 774D00 90FB92 0076FF D5FF00 FF937E 6A826C FF029D FE8900 7A4782 7E2DD2 85A900 FF0056 A42400 00AE7E 683D3B BDC6FF 263400 BDD393 00B917 9E008E 001544 C28C9F FF74A3 01D0FF 004754 E56FFE 788231 0E4CA1 91D0CB BE9970 968AE8 BB8800 43002C DEFF74 00FFC6 FFE502 620E00 008F9C 98FF52 7544B1 B500FF 00FF78 FF6E41 005F39 6B6882 5FAD4E A75740 A5FFD2 FFB167 009BFF E85EBE )
+
+NUMCOLORS=${#LINECOLORS[@]}
+colorindex=0
 
 BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
@@ -98,10 +105,14 @@ timespan=$((interval * 86400))
 # guidrule=
 # defsandlines=
 # for (( i=0; i < ${numcpus}; i++ )); do
-#   defsandlines="${defsandlines} DEF:cpu${i}=${datafile}:cpu${i}:MAX LINE1:cpu${i}${LINECOLORS[$i]}:\"cpu${i}\""
+#   (( colorindex = i % NUMCOLORS )) # If we run out of colors, start over
+#   defsandlines="${defsandlines} DEF:cpu${i}=${datafile}:cpu${i}:MAX LINE1:cpu${i}${LINECOLORS[$colorindex]}:\"cpu${i}\""
 # done
-# for i in ${drivedevs}; do
-#   defsandlines="${defsandlines} DEF:${i}=${datafile}:${i}:MAX LINE1:${i}${LINECOLORS[$i]}:\"${i}\""
+# i=0
+# for drdev in ${drivedevs}; do
+#   (( colorindex = ( i + numcpus ) % NUMCOLORS )) # Don't reuse the cpu colors unless we have to
+#   defsandlines="${defsandlines} DEF:${drdev}=${datafile}:${drdev}:MAX LINE1:${drdev}${LINECOLORS[$colorindex]}:\"${drdev}\""
+#   (( i = i + 1 ))
 # done
 # write_graph_to_disk
 
@@ -111,7 +122,8 @@ defsandlines=
 title="Temperature: All CPUs, ${interval} minute interval"
 guidrule=
 for (( i=0; i < ${numcpus}; i++ )); do
-  defsandlines="${defsandlines} DEF:cpu${i}=${datafile}:cpu${i}:MAX LINE1:cpu${i}#${LINECOLORS[$i]}:cpu${i}"
+  (( colorindex = i % NUMCOLORS )) # If we run out of colors, start over
+  defsandlines="${defsandlines} DEF:cpu${i}=${datafile}:cpu${i}:MAX LINE1:cpu${i}#${LINECOLORS[$colorindex]}:cpu${i}"
 done
 write_graph_to_disk
 
@@ -120,15 +132,17 @@ outputfilename=drives
 defsandlines=
 title="Temperature: All Drives, ${interval} minute interval"
 guidrule=HRULE:${SAFETEMPLINE}#FF0000:Max-safe-temp:dashes
-for i in ${drivedevs}; do
-  drivenum=${i#ada*}
-  defsandlines="${defsandlines} DEF:${i}=${datafile}:${i}:MAX LINE1:${i}#${LINECOLORS[$drivenum]}:${i}"
+i=0
+for drdev in ${drivedevs}; do
+  (( colorindex = i % NUMCOLORS )) # If we run out of colors, start over
+  defsandlines="${defsandlines} DEF:${drdev}=${datafile}:${drdev}:MAX LINE1:${drdev}#${LINECOLORS[$colorindex]}:${drdev}"
+  (( i = i + 1 ))
 done
 write_graph_to_disk
 
 # # Output graphs of each cpu
 # for (( i=0; i < ${numcpus}; i++ )); do
-#   defsandlines="DEF:cpu${i}=${datafile}:cpu${i}:MAX LINE1:cpu${i}${LINECOLORS[$i]}:\"cpu${i}\""
+#   defsandlines="DEF:cpu${i}=${datafile}:cpu${i}:MAX LINE1:cpu${i}#000000:\"cpu${i}\""
 #   outputfilename=cpu${i}
 #   title="Temperature: CPU ${i}, ${interval} minute interval"
 #   guidrule=
@@ -138,10 +152,11 @@ write_graph_to_disk
 # # Output graphs of each drive
 # for i in ${drivedevs}; do
 #   drivenum=${i#ada*}
-#   defsandlines="DEF:${i}=${datafile}:${i}:MAX LINE1:${i}${LINECOLORS[$drivenum]}:\"${i}\""
+#   defsandlines="DEF:${i}=${datafile}:${i}:MAX LINE1:${i}#000000:\"${i}\""
 #   outputfilename=drive-${i}
 #   guidrule="HRULE:${SAFETEMPLINE}#FF0000"
 #   title="Temperature: Drive ${i}, ${interval} minute interval"
 #   write_graph_to_disk
 # done
+
 
