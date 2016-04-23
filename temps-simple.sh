fe683e1c73ee2442d005890f4e10c0587f32e97e
Seren <git@seren.net>
2016-04-22 23:08:34 -0700

    Incled 'da' devices, and print cpu temps first

--- temps-simple.sh
+++ temps-simple.sh
@@ -7,12 +7,12 @@ if [ "$(id -u)" != "0" ]; then
   exit 1
 fi
 
-for i in $(/sbin/sysctl -n kern.disks | awk '{for (i=NF; i!=0 ; i--) if(match($i, '/ada/')) print $i }' );
+/sbin/sysctl -a |egrep -E "cpu\.[0-9]+\.temp"
+for i in $(/sbin/sysctl -n kern.disks | awk '{for (i=NF; i!=0 ; i--) if(match($i, '/da/')) print $i }' )
 do
  DevTemp=`/usr/local/sbin/smartctl -a /dev/$i | awk '/194 Temperature_Celsius/{print $0}' | awk '{print $10 "C"}'`;
  #DevSerNum=`/usr/local/sbin/smartctl -a /dev/$i | awk '/Serial Number:/{print $0}' | awk '{print $3}'`;
  #DevName=`/usr/local/sbin/smartctl -a /dev/$i | awk '/Device Model:/{print $0}' | awk '{print $3}'`;
  echo "$i - $DevTemp $DevSerNum $DevName";
 done
-/sbin/sysctl -a |egrep -E "cpu\.[0-9]+\.temp"
 
